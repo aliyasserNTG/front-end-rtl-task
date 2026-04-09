af65775146dd8ab7943fba1ab9a4545b0c61c158
Ali Yasser <aliyasser@ntgclarity.com>
2026-04-09 15:23:13 +0200

    Buttons redecirect to respective routerLinks "services, form"

--- front-end-rtl-task/src/app/app.routes.ts
+++ front-end-rtl-task/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomeScreenComponent} from './components/screens/home-screen-component/home-screen-component';
-import {FormScreenComponent} from './components/screens/form-screen-component/form-screen-component';
 import {ServicesScreenComponent} from './components/screens/services-screen-component/services-screen-component';
+import {FormScreenComponent} from './components/screens/form-screen-component/form-screen-component';
 
 export const routes: Routes = [
   {
@@ -9,11 +9,11 @@ export const routes: Routes = [
     component: HomeScreenComponent
   },
   {
-    path: 'form',
-    component: FormScreenComponent
-  },
-  {
     path: 'services',
     component: ServicesScreenComponent
+  },
+  {
+    path: 'form',
+    component: FormScreenComponent
   },
 ];
